51e5a21fb13c5af1ddd631e817840a9fd7724174
Martin Ivanko <martin.ivanko1238@gmail.com>
2019-08-16 15:32:13 +0200

    a lot of

--- src/App.js
+++ src/App.js
@@ -1,64 +1,96 @@
 import React, { Component } from "react";
+import http from "./services/httpService";
+import { ToastContainer } from "react-toastify";
+import config from "./config.json";
+
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
 class App extends Component {
-  state = {
-    posts: []
-  };
+    state = {
+        posts: []
+    };
+    async componentDidMount() {
+        const { data: posts } = await http.get(config.apiEndPoint);
+        this.setState({ posts });
+    }
 
-  handleAdd = () => {
-    console.log("Add");
-  };
+    handleAdd = async () => {
+        const obj = { title: "a", body: "b" };
+        const { data: post } = await http.post(config.apiEndPoint, obj);
 
-  handleUpdate = post => {
-    console.log("Update", post);
-  };
+        const posts = [post, ...this.state.posts];
+        this.setState({ posts });
+    };
 
-  handleDelete = post => {
-    console.log("Delete", post);
-  };
+    handleUpdate = async post => {
+        post.title = "UPDATE";
 
-  render() {
-    return (
-      <React.Fragment>
-        <button className="btn btn-primary" onClick={this.handleAdd}>
-          Add
-        </button>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Update</th>
-              <th>Delete</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.posts.map(post => (
-              <tr key={post.id}>
-                <td>{post.title}</td>
-                <td>
-                  <button
-                    className="btn btn-info btn-sm"
-                    onClick={() => this.handleUpdate(post)}
-                  >
-                    Update
-                  </button>
-                </td>
-                <td>
-                  <button
-                    className="btn btn-danger btn-sm"
-                    onClick={() => this.handleDelete(post)}
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </React.Fragment>
-    );
-  }
+        const posts = [...this.state.posts];
+        const index = posts.indexOf(post);
+        posts[index] = { ...post };
+        this.setState({ posts });
+
+        await http.put(config.apiEndPoint + "/" + post.id, post);
+    };
+
+    handleDelete = async post => {
+        const originalPosts = this.state.posts;
+
+        const posts = this.state.posts.filter(p => p.id !== post.id);
+        this.setState({ posts });
+
+        try {
+            await http.delete("a" + config.apiEndPoint + "/999" + post.id);
+        } catch (ex) {
+            if (ex.response && ex.response.status === 404)
+                alert("Something failed while deleting a post !");
+            this.setState({ posts: originalPosts });
+        }
+    };
+
+    render() {
+        return (
+            <React.Fragment>
+                <ToastContainer />
+                <button className="btn btn-primary" onClick={this.handleAdd}>
+                    Add
+                </button>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Title</th>
+                            <th>Update</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.posts.map(post => (
+                            <tr key={post.id}>
+                                <td>{post.title}</td>
+                                <td>
+                                    <button
+                                        className="btn btn-info btn-sm"
+                                        onClick={() => this.handleUpdate(post)}
+                                    >
+                                        Update
+                                    </button>
+                                </td>
+                                <td>
+                                    <button
+                                        className="btn btn-danger btn-sm"
+                                        onClick={() => this.handleDelete(post)}
+                                    >
+                                        Delete
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </React.Fragment>
+        );
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Raven from "raven-js";
 import ReactDOM from "react-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
+
+Raven.config("https://34d81846b978437885cd43891ef1aea8@sentry.io/1532699", {
+    release: "0-0-0",
+    enviroment: "development-test"
+}).install();
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
